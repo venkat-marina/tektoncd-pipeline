497ee6fb17b3bf67cc29503e0568c9b116d6f9ef
Chmouel Boudjnah <chmouel@redhat.com>
2019-12-13 10:43:48 +0100

    Add openshift specific files.

---
+++ Makefile
@@ -0,0 +1,86 @@
+#This makefile is used by ci-operator
+
+CGO_ENABLED=0
+GOOS=linux
+CORE_IMAGES=./cmd/controller ./cmd/entrypoint ./cmd/kubeconfigwriter ./cmd/webhook ./cmd/imagedigestexporter ./cmd/pullrequest-init
+CORE_IMAGES_WITH_GIT=./cmd/creds-init ./cmd/git-init
+ADDN_IMAGES=./vendor/github.com/GoogleCloudPlatform/cloud-builders/gcs-fetcher/cmd/gcs-fetcher
+ALL_IMAGES=$(CORE_IMAGES) $(CORE_IMAGES_WITH_GIT) $(ADDN_IMAGES)
+
+##
+# You need to provide a RELEASE_VERSION when using targets like `push-image`, you can do it directly
+# on the command like this: `make push-image RELEASE_VERSION=0.4.0`
+RELEASE_VERSION=
+REGISTRY_CI_URL=registry.svc.ci.openshift.org/openshift/tektoncd-v$(RELEASE_VERSION):tektoncd-pipeline
+REGISTRY_RELEASE_URL=quay.io/openshift-pipeline/tektoncd-pipeline
+
+# Install core images
+install: installuidwrapper
+	go install $(ALL_IMAGES)
+.PHONY: install
+
+# Run E2E tests on OpenShift
+test-e2e: check-images
+	./openshift/e2e-tests-openshift.sh
+.PHONY: test-e2e
+
+# Make sure we have all images in the makefile variable or that would be a new
+# binary that needs to be added
+check-images:
+	@notfound="" ;\
+	for cmd in ./cmd/*;do \
+		[[ ! "$(ALL_IMAGES)" == *$$cmd* ]] && { \
+			notfound="$$notfound $$cmd " ;\
+		} \
+	done ;\
+	test -z "$$notfound" || { \
+		echo "*ERROR*: Could not find $$notfound in the Makefile variables ALL_IMAGES" ;\
+		echo "" ;\
+		echo "If it it's a new binary that was added upstream, then do the following :" ;\
+		echo "- Add the binary to openshift/release like this: https://git.io/fj18c" ;\
+		echo "- Add to the CORE_IMAGES variables in the Makefile" ;\
+		echo "- Generate the dockerfiles by running 'make generate-dockerfiles'" ;\
+		echo "- Commit and PR these to 'openshift/release-next' remote/branch and 'openshift/master'" ;\
+		echo "- Make sure the images are added in the nightly quay jobs https://git.io/Jeu1I" ;\
+		echo "" ;\
+		exit 1 ;\
+	}
+	@notfound="" ;\
+	for cmd in $(ALL_IMAGES);do \
+		[[ -d $$cmd ]] || { \
+			echo "*ERROR*: $$cmd seems to have been removed from upstream" ;\
+			echo "" ;\
+			echo "- Remove the image name in one of the Makefile variable" ;\
+			echo "- Remove the image from the openshfit/release nightly https://git.io/Jez1j and variant https://git.io/JezMv" ;\
+			echo "- Remove the directory from openshift/ci-operator/tekton-images/" ;\
+			echo "- Remove the image from the nightly quay job: https://git.io/Jeu1I" ;\
+			echo "" ;\
+			exit 1 ;\
+		} ;\
+	done
+.PHONY: check-images
+
+# Generate Dockerfiles used by ci-operator. The files need to be committed manually.
+generate-dockerfiles:
+	@./openshift/ci-operator/generate-dockerfiles.sh openshift/ci-operator/Dockerfile.in openshift/ci-operator/tekton-images $(CORE_IMAGES)
+	@./openshift/ci-operator/generate-dockerfiles.sh openshift/ci-operator/Dockerfile.in openshift/ci-operator/tekton-images $(ADDN_IMAGES)
+	@./openshift/ci-operator/generate-dockerfiles.sh openshift/ci-operator/Dockerfile-git.in openshift/ci-operator/tekton-images $(CORE_IMAGES_WITH_GIT)
+.PHONY: generate-dockerfiles
+
+# NOTE(chmou): Install uidwraper for launching some binaries with fixed uid
+UIDWRAPPER_PATH=./openshift/ci-operator/uidwrapper
+installuidwrapper: $(UIDWRAPPER_PATH)
+	install -m755 $(UIDWRAPPER_PATH) $(GOPATH)/bin/
+
+# Generates a ci-operator configuration for a specific branch.
+generate-ci-config:
+	./openshift/ci-operator/generate-ci-config.sh $(BRANCH) > ci-operator-config.yaml
+.PHONY: generate-ci-config
+
+# Generate an aggregated knative yaml file with replaced image references
+generate-release:
+	@test $(RELEASE_VERSION) || { echo "You need to set the RELEASE_VERSION on the command line i.e: make RELEASE_VERSION=0.4.0"; exit 1;}
+	@./openshift/release/generate-release.sh v$(RELEASE_VERSION)
+.PHONY: generate-release
+
+.PHONY: push-image
---
+++ openshift/resolve-yamls.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+TMP=$(mktemp /tmp/.mm.XXXXXX)
+clean() { rm -f ${TMP}; }
+trap clean EXIT
+
+# Take all yamls in $dir and generate a all in one yaml file with resolved registry image/tag
+function resolve_resources() {
+  local dir=$1
+  local resolved_file_name=$2
+  local ignores=$3
+  local registry_prefix=$4
+  local image_tag=${5}
+
+  # This would get only one set of truth from the Makefile for the image lists
+  #
+  # % grep "^CORE_IMAGES" Makefile
+  # CORE_IMAGES=./cmd/controller ./cmd/entrypoint ./cmd/kubeconfigwriter ./cmd/nop ./cmd/webhook ./cmd/imagedigestexporter
+  # CORE_IMAGES_WITH_GIT=./cmd/creds-init ./cmd/git-init
+  # to:
+  #  % grep '^CORE_IMAGES' Makefile|sed -e 's/.*=//' -e 's,./cmd/,,g'|tr -d '\n'|sed -e 's/ /|/g' -e 's/^/(/' -e 's/$/)\n/'
+  # (controller|entrypoint|gsutil|kubeconfigwriter|nop|webhook|imagedigestexportercreds-init|git-init)
+  local image_regexp=$(grep '^CORE_IMAGES' $(git rev-parse --show-toplevel)/Makefile| \
+                           sed -e 's/.*=//' -e 's,./cmd/,,g'|tr '\n' ' '| \
+                           sed -e 's/ /|/g' -e 's/^/(/' -e 's/|$/)\n/')
+
+  >$resolved_file_name
+  for yaml in $(find $dir -name "*.yaml" | grep -vE $ignores | sort); do
+    echo "---" >>$resolved_file_name
+    if [[ -n ${image_tag} ]];then
+        # This is a release format the output would look like this :
+        # quay.io/openshift-pipeline/tektoncd-pipeline-bash:$image_tag
+        #
+        # NOTE(chmou): We are using our owns stuff for external images :
+        #
+        # tianon/true => openshift/ci-operator/tekton-images/nop/Dockerfile
+        # busybox => registry.access.redhat.com/ubi8/ubi-minimal:latest \
+        #
+        # TODO(chmou): gcs-util is still going external and not ubi based
+        sed -e "s%tianon/true%${registry_prefix}-nop:${image_tag}%" \
+            -e "s%busybox%registry.access.redhat.com/ubi8/ubi-minimal:latest%" \
+            -e "s%\(.* image: \)\(github.com\)\(.*\/\)\(.*\)%\1 ${registry_prefix}-\4:${image_tag}%" $yaml \
+            -r -e "s,github.com/tektoncd/pipeline/cmd/${image_regexp},${registry_prefix}-\1:${image_tag},g" \
+            > ${TMP}
+     else
+        # This is CI which get built directly to the user registry namespace i.e: $OPENSHIFT_BUILD_NAMESPACE
+        # The output would look like this :
+        # internal-registry:5000/usernamespace:tektoncd-pipeline-bash
+        #
+        # note: test image are images only used for testing not for releases
+        #
+        # NOTE(chmou): We are using our owns stuff for external images :
+        #
+        # tianon/true => openshift/ci-operator/tekton-images/nop/Dockerfile
+        # busybox => registry.access.redhat.com/ubi8/ubi-minimal:latest \
+        #
+        # TODO(chmou): gcs-util is still going external and not ubi based
+         sed -e "s%tinaon/true%${registry_prefix}:tektoncd-pipeline-nop%" \
+             -e "s%busybox%registry.access.redhat.com/ubi8/ubi-minimal:latest%" \
+             -e 's%\(.* image: \)\(github.com\)\(.*\/\)\(test\/\)\(.*\)%\1\2 \3\4test-\5%' $yaml \
+             -e "s%\(.* image: \)\(github.com\)\(.*\/\)\(.*\)%\1 ""$registry_prefix"'\:tektoncd-pipeline-\4%'  \
+             -re "s,github.com/tektoncd/pipeline/cmd/${image_regexp},${registry_prefix}:tektoncd-pipeline-\1,g" \
+             > ${TMP}
+    fi
+
+    # Adding the labels: openshift.io/cluster-monitoring on Namespace to add the cluster-monitoring
+    # See: https://docs.openshift.com/container-platform/4.1/logging/efk-logging-deploying.html
+    grep -q "kind: Namespace" ${TMP} && sed -i '/^metadata:/a \ \ labels:\n\ \ \ \ openshift.io/cluster-monitoring:\ \"true\"' ${TMP}
+
+    cat ${TMP} >> $resolved_file_name
+
+    echo >>$resolved_file_name
+  done
+
+  # handle additional images which are not build from ./cmd (images not in $CORE_IMAGES)
+  if [[ -n ${image_tag} ]];then
+      sed -i -r -e "s,github.com/tektoncd/pipeline/vendor/github.com/GoogleCloudPlatform/cloud-builders/gcs-fetcher/cmd/gcs-fetcher,${registry_prefix}-gcs-fetcher:${image_tag},g" $resolved_file_name
+   else
+      sed -i -r -e "s,github.com/tektoncd/pipeline/vendor/github.com/GoogleCloudPlatform/cloud-builders/gcs-fetcher/cmd/gcs-fetcher,${registry_prefix}:tektoncd-pipeline-gcs-fetcher,g" $resolved_file_name
+  fi
+}
+
+function generate_pipeline_resources() {
+    local output_file=$1
+    local image_prefix=$2
+    local image_tag=$3
+
+    resolve_resources config/ $output_file noignore $image_prefix $image_tag
+
+    # Appends addon configs such as prometheus monitoring config
+    for yaml in $(find openshift/addons/ -name "*.yaml" | sort); do
+      echo "---" >> $output_file
+      cat ${yaml} >> $output_file
+    done
+}
