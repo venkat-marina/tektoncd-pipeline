d907d9668046402897c0e4ae54eeaa7f52ef269e
Piyush Garg <pgarg@redhat.com>
2020-05-20 23:00:39 +0530

    Fix image replacement
    This will fix the image replacement as the
    busybox and tianon/true image has sha also now

--- openshift/resolve-yamls.sh
+++ openshift/resolve-yamls.sh
@@ -32,10 +32,10 @@ function resolve_resources() {
         #
         # NOTE(chmou): We are using our owns stuff for external images :
         #
-        # tianon/true => openshift/ci-operator/tekton-images/nop/Dockerfile
-        # busybox => registry.access.redhat.com/ubi8/ubi-minimal:latest \
-        sed -e "s,ko://,,g"  -e "s%tianon/true%${registry_prefix}-nop:${image_tag}%" \
-            -e "s%busybox%registry.access.redhat.com/ubi8/ubi-minimal:latest%" \
+        # tianon/true@sha* => openshift/ci-operator/tekton-images/nop/Dockerfile
+        # busybox@sha* => registry.access.redhat.com/ubi8/ubi-minimal:latest \
+        sed -e "s,ko://,,g"  -e "s%tianon/true.*\(\"\)%${registry_prefix}-nop:${image_tag}%\1" \
+            -e "s%busybox.*\(\"\)%registry.access.redhat.com/ubi8/ubi-minimal:latest%\1" \
             -e "s%\(.* image:\)\(github.com\)\(.*\/\)\(.*\)%\1 ${registry_prefix}-\4:${image_tag}%" $yaml \
             -r -e "s,github.com/tektoncd/pipeline/cmd/${image_regexp},${registry_prefix}-\1:${image_tag},g" \
             > ${TMP}
@@ -48,10 +48,10 @@ function resolve_resources() {
         #
         # NOTE(chmou): We are using our owns stuff for external images :
         #
-        # tianon/true => openshift/ci-operator/tekton-images/nop/Dockerfile
-        # busybox => registry.access.redhat.com/ubi8/ubi-minimal:latest \
-         sed -e "s,ko://,,g" -e "s%tinaon/true%${registry_prefix}:tektoncd-pipeline-nop%" \
-             -e "s%busybox%registry.access.redhat.com/ubi8/ubi-minimal:latest%" \
+        # tianon/true@sha* => openshift/ci-operator/tekton-images/nop/Dockerfile
+        # busybox@sha* => registry.access.redhat.com/ubi8/ubi-minimal:latest \
+         sed -e "s,ko://,,g" -e "s%tinaon/true.*\(\"\)%${registry_prefix}:tektoncd-pipeline-nop%\1" \
+             -e "s%busybox.*\(\"\)%registry.access.redhat.com/ubi8/ubi-minimal:latest%\1" \
              -e 's%\(.* image:\)\(github.com\)\(.*\/\)\(test\/\)\(.*\)%\1\2 \3\4test-\5%' $yaml \
              -e "s%\(.* image:\)\(github.com\)\(.*\/\)\(.*\)%\1 ""$registry_prefix"'\:tektoncd-pipeline-\4%'  \
              -re "s,github.com/tektoncd/pipeline/cmd/${image_regexp},${registry_prefix}:tektoncd-pipeline-\1,g" \
